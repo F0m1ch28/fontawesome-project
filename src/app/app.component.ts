5cee2135f953d2d32fde870eb34a33486672f491
Illia Fomenko <fomenko.i@smtech.ru>
2024-04-13 07:33:22 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faAd, faAnchor, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-fontawesome-project';
+  currentIcon: IconDefinition = faCoffee;
+  icons = [faCoffee, faAd, faAnchor];
+  iconEnterState = '';
+
+  showRandomIcon(): void {
+    this.iconEnterState = 'icon-enter';
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * this.icons.length);
+      this.currentIcon = this.icons[randomIndex];
+      this.iconEnterState = 'icon-enter-active';
+    }, 3000);
+  }
 }
